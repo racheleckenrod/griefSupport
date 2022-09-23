df4550737b3d0136afb9be26880a3aaa83e5e24c
racheleckenrod <rachel@racheleckenrod.com>
2022-09-23 16:30:32 -0500

    contain mess of mixed up code

--- server.js
+++ server.js
@@ -14,7 +14,22 @@ const {
   getRoomUsers,
 } = require("./utils/users");
 
+// from BUB
+// const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const methodOverride = require("method-override");
+const flash = require("express-flash");
+const logger = require("morgan");
+const connectDB = require("./config/database");
+const mainRoutes = require("./routes/main");
+const postRoutes = require("./routes/posts");
+const commentRoutes = require("./routes/comments");
+
+// const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
@@ -82,7 +97,54 @@ io.on("connection", (socket) => {
     }
   });
 });
+// Passport config
+require("./config/passport")(passport);
 
+//Connect To Database
+connectDB();
+
+//Using EJS for views
+app.set("view engine", "ejs");
+
+//Static Folder
+app.use(express.static("public"));
+
+//Body Parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Logging
+app.use(logger("dev"));
+
+//Use forms for put / delete
+app.use(methodOverride("_method"));
+
+// Setup Sessions - stored in MongoDB
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Use flash messages for errors, info, ect...
+app.use(flash());
+
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+app.use("/post", postRoutes);
+app.use("/comment", commentRoutes);
+
+//Server Running
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
