7f1505fd88c1786905a242238a086b70b4942a35
racheleckenrod <rachel@racheleckenrod.com>
2022-09-29 07:48:41 -0500

    experiment is successfull

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 // Set static folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static("public"));
 
 const botName = "Grief Support Bot";
 
